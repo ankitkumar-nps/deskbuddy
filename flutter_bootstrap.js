3be215dedb865e9a47f68dd61aa239e89c390058
ankitkumar-nps <ankitkumar-nps@users.noreply.github.com>
2026-06-04 10:39:32 +0530

    Fix: Correct base-href for GitHub Pages deployment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2428934434" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "422913689" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
